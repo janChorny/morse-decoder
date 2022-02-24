bfab5f74f02af3cb9c938dfd2fc8b099d362d68e
janChorny <janchorny@tut.by>
2022-02-24 12:26:22 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -39,6 +39,23 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+	 let result = '';
+	 let word = '';
+	 for (let i = 0; i < expr.length; i+=10){
+		 word = expr.slice(i, i + 10);
+		 if (word == '**********'){ result += ' '}
+		 else {
+			 let letter = '';
+			 let keyLetter = '';
+			 for (let j = 0; j < word.length; j+=2){
+				 letter = word.slice(j, j + 2)
+				 if (letter == '10'){ keyLetter += '.'}
+				 if (letter == '11') { keyLetter += '-'}
+			 }
+			 result += MORSE_TABLE[keyLetter];
+		 }
+	 }
+	 return result;
 }
 
 module.exports = {
